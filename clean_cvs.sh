db5898833f50719a2303e0eee7ee9c0ed6e1c271
Carsten Teibes <dev@f4ke.de>
2026-03-19 23:18:10 +0100

    Drop old stuff: buildsystem, installers, editor

--- clean_cvs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# remove CVSROOT and .cvsignore everywhere
-
-git filter-branch --force --tree-filter '
-rm -rf CVSROOT
-find . -name ".cvsignore" -delete
-' --prune-empty -- --all
--- user_gitlab.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# sets user name and credential useHttpPath
-
-git config user.name Trilarion
-git config user.email gitlab@twelvepm.de
-git config credential.useHttpPath true
